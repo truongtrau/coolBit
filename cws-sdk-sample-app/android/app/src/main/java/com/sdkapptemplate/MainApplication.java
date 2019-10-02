75a1e3eb65a8e5efa5895488ac90ae0be92bca12
truong.tran <truongtran@truong.tran>
2019-10-02 11:01:27 +0700

    init sample app

---
+++ cws-sdk-sample-app/android/app/src/main/java/com/sdkapptemplate/MainApplication.java
@@ -0,0 +1,54 @@
+package com.sdkapptemplate;
+
+import android.app.Application;
+
+import com.bitgo.randombytes.RandomBytesPackage;
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+import com.oblador.keychain.KeychainPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+import it.innove.BleManagerPackage;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      packages.add(new RandomBytesPackage());
+      packages.add(new KeychainPackage());
+      packages.add(new BleManagerPackage());
+      return packages;
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
